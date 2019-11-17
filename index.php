beb0e146ca4e9087b6951265f4c3b514a9adda1c
Triwit <lis160641@gmail.com>
2019-11-17 14:06:05 +0700

    text04

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 Hello my name is Lee. 
 How are you to day.
+
+public class HanoiTower {
+    static  int i=0;
+  public static void main(String[] args) {
+    hanoi(7, "A", "B", "C");
+  }
+  public static void hanoi(int n, String a, String b, String c) {
+      
+    if (n == 0) return;
+    hanoi(n - 1, a, c, b);
+    i=i+1;
+    System.out.println(i + " move " + n + " : " + a + " -> " + c);
+    hanoi(n - 1, b, a, c);
+  }
+}
+
